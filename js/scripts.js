49ebfff2d0ba0dab522c9f60212cdece681363e3
Grace Lee <gracexsnoopy@gmail.com>
2022-09-22 22:30:51 -0700

    Display all toppings user selects

--- js/scripts.js
+++ js/scripts.js
@@ -10,16 +10,16 @@ function Pizza(toppings, size) {
 
 Pizza.prototype.sizeCost = function() {
   if (this.size === "small") {
-    this.price += 2; //12, 13
+    this.price += 2; 
     return this.price;
   } else if (this.size === "medium") {
-    this.price += 4; //14, 15
+    this.price += 4; 
     return this.price; 
   } else if (this.size === "large") {
-    this.price += 6; //16, 17
+    this.price += 6; 
     return this.price; 
   } else {
-    this.price += 8; //18, 19
+    this.price += 8; 
     return this.price; 
   }
 }
@@ -28,6 +28,8 @@ Pizza.prototype.totalCost = function() {
   return 1 + this.sizeCost();
 }
 
+
+
 window.addEventListener("load", function() {
   let form = document.querySelector("form")
   form.addEventListener("submit", handleFormSubmission)
@@ -35,14 +37,22 @@ window.addEventListener("load", function() {
 
 function handleFormSubmission(event) {
   event.preventDefault();
-  
-  const toppingSelection = document.querySelector("input[name='toppings-1']:checked").value;
+
+  const toppingArray = []
+  const toppingSelection = document.getElementsByName("topping");
+  for (let i = 0; i < toppingSelection.length; i++) {
+    if (toppingSelection[i].checked === true) {
+      toppingArray.push(toppingSelection[i].value);
+    }
+  }
   const sizeSelection = document.querySelector("input[name='size']:checked").value;
 
   let myPizza = new Pizza(toppingSelection, sizeSelection);
   const pizzaTotalPrice = myPizza.totalCost();
+  console.log(myPizza.toppings)
+  console.log(myPizza.size)
 
   document.querySelector("span.pizza-size").innerText = sizeSelection;
-  document.querySelector("span.toppings-list").innerText = toppingSelection;
+  document.querySelector("span.toppings-list").innerText = toppingArray.join(", ");
   document.querySelector("span.total-cost").innerText = "$" + pizzaTotalPrice;
 }
